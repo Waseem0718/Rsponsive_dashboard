071f725a4d215256c27d469537dd823f0cb4236d
Waseemullah K <122109701+Waseem0718@users.noreply.github.com>
2024-09-14 19:52:43 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,32 @@
+const orders = [
+    {
+        productName: "Foldable Mini drone",
+        productNumber: "23221",
+        paymentStatus: "Refunded",
+        shipping: "Declined"
+    },
+    {
+        productName: "Lavender MX3 drone",
+        productNumber: "56221",
+        paymentStatus: "Due",
+        shipping: "Pending"
+    },
+    {
+        productName: "Crystal WQ7 drone",
+        productNumber: "76311",
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    },
+    {
+        productName: "Garuda Drone M3",
+        productNumber: "98737",
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    },
+    {
+        productName: "GPS 4k Drone",
+        productNumber: "87122",
+        paymentStatus: "Refunded",
+        shipping: "Declined"
+    }
+]
